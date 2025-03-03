2b9c4ed23d16133409259b9d8e16b06b8bed7fdc
Peter Kalinowski <petekal1323@gmail.com>
2025-03-03 17:45:31 -0500

    string interpolation exercise

---
+++ variables-Exercise/variables-Exercise/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace variables_Exercise;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string dogName = "Ralph";
+        int dogAge = 5;
+        char dogSex = 'M';
+        double dogWeight = 65.22;
+        decimal dogHeight = 2.5m;
+        
+        Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old. He is a {dogSex}(male) dog. He weighs {dogWeight} pounds. He is {dogHeight} feet tall.");
+    }
+}
